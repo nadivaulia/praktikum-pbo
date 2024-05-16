34419210804fed4d87d6e7328b0a9f7a2af6c75e
nadivaulia <160684665+nadivaulia@users.noreply.github.com>
2024-05-16 22:37:40 +0700

    Update BangunDatarGeneric.java

--- Praktikum 8/Modul/BangunDatarGeneric.java
+++ Praktikum 8/Modul/BangunDatarGeneric.java
@@ -1,11 +1,11 @@
-public class BangunDatarGeneric <T extends BangunDatar>{
-    private T bangunDatar;
+public class BangunDatarGeneric <T2 extends BangunDatar>{
+    private T2 bangunDatar;
 
-    public void set(T tipeBangunDatar){
+    public void set(T2 tipeBangunDatar){
         bangunDatar = tipeBangunDatar;
     }
 
-    public T get(){
+    public T2 get(){
         return bangunDatar;
     }
 
@@ -13,3 +13,9 @@ public class BangunDatarGeneric <T extends BangunDatar>{
         return bangunDatar.hitungKeliling();
     }
 }
+
+// Setelah mengganti T menjadi T2, tidak terjadi perubahan
+// fungsionalitas dari kode tersebut, karena T adalah nama
+// placeholder atau parameter tipe generik dalam Java
+// yang bisa diganti dengan nama apapun. Tipe datanya tetap
+// berupa BangunDatar.
